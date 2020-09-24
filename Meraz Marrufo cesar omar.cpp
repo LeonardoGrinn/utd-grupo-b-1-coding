0c82061c8c81a0eb2452f11f9230fac0eaacab3a
Meraz Marrufo cesar omar <71277678+merazmarrufocesaromar@users.noreply.github.com>
2020-09-24 16:07:55 -0500

    cesar omar código C++

---
+++ Meraz Marrufo cesar omar.cpp
@@ -0,0 +1,18 @@
+//Libreria que se va a estudiar
+#include <stdlib.h> //Libreria estandar para realisar tareas
+#include <stdlib.h> //Libreria estandar para la entrada y salida de informacion en pantalla.
+
+Meraz Marrufo cesar omar 
+
+//Funcion principal
+int main(); {//inicio del programa
+
+//Estructura del programa
+printf("Mi crrera en energias renovables 1b\n");
+printf("Linea 2 \n");
+system("pause\n");
+return 0;
+
+}// Fin de programa.
+
+
