c0040f51b01f39f4dd5c1611b754e714e298927a
DanielB1202 <71311563+DanielB1202@users.noreply.github.com>
2020-09-24 13:23:49 -0500

    Add files via upload

---
+++ benitez.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+printf("Hola soy daniel \n");
+printf("y tomo mucha agua \n");
+printf("asi es \n");
+
+system("pause");
+return 0;
+
+}
+
+
