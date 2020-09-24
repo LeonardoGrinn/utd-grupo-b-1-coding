6fbba26a3bf3358b535f637a6976d71ba1d4e73b
Bryan de la Rosa <bryanManuel5600@gmail.com>
2020-09-24 14:55:33 -0500

    PRACTICA C++
    practica c++

---
+++ De la Rosa-Jimenez-Bryan.cpp
@@ -0,0 +1,16 @@
+
+// Libreria que se va a utilizar.
+#include <stdlib.h> // Libreria estandar para realizar funciones del sistema.
+#include <stdio.h> // Libreri8a estandar para la entrada y salida de informacion en pantalla.
+
+// Funcion principal
+int main() { //Inicio de programa
+
+   // Estructura del programa.
+   printf ("Hola mundo  \n");
+   printf ("Practica c++ \n");
+
+   system ("pause");
+   return 0;
+
+} // Fin del programa 
