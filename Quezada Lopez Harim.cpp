25bcda48e2dd72b1c0f60d4579479916ee24b0a4
QuezadaLopezHarim <harimquezada9@gmail.com>
2020-09-25 10:48:34 -0500

    Quezada Lopez Harim
    practica 1 c++

---
+++ Quezada Lopez Harim.cpp
@@ -0,0 +1,12 @@
+#include<stdlib.h> //libreria estandar para relaizar fuciones del sitema
+#include<stdio.h> //libreria estandar para la entrada y salida de informacion en pantalla
+
+//funcion principal
+int main()
+{// inicio del programa
+printf ("Hola mundo soy Harim Quezada");
+
+system("pause");
+return 0;
+} // fin del programa
+
