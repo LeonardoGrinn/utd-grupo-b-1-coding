6629cca21fe352773ce8e3fc1e4e4eb5c1600d1f
Cepeda Rodriguez Jennifer <jennifercepeda996@gmail.com>
2020-10-04 20:16:32 -0500

    condicional-if.cpp
    Jennifer Cepeda Rodriguez

---
+++ condicional-if.cpp
@@ -0,0 +1,20 @@
+#include<iostream> 
+using namespace std;
+
+int main() {
+
+ int firstNumber = 10, secondNumber;
+
+ cout<<"Digita un numero: ";
+ cin>>secondNumber;
+
+
+  if (firstNumber == secondNumber) {
+     cout<<"Ambos numeros son iguales";
+    }else
+      cout<<"Los numerosson distintos";
+    
+  
+    return 0;
+
+}
